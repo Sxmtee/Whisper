17c3cca523f7db68a244a21f43d243f48ac50e8f
Sxmtee <somtoo76@gmail.com>
2022-11-21 01:03:18 +0100

    authscreen

--- lib/Screens/authScreen.dart
+++ lib/Screens/authScreen.dart
@@ -16,10 +16,45 @@ class _AuthScreenState extends State<AuthScreen> {
       body: Center(
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets/Images/chatzo.png"))),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/Images/chatzo.png"))),
+              ),
+            ),
+            Text(
+              "Whisper",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: (() {}),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 50,
+                      width: 50,
+                      child: Image.asset("assets/Images/googlelogo.png")),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text(
+                    "Sign in with Google",
+                    style: TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 12))),
+            ),
+            SizedBox(
+              height: 30,
             )
           ],
         ),
